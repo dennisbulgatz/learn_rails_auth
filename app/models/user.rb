335ed65a21eb7f2bc7456f20ab89af4ff4081e81
Adam Bulgatz <adam@bulgatz.com>
2015-02-15 03:23:54 +0000

    Added validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ActiveRecord::Base
+  attr_accessible :email, :password, :password_confirmation
+  
+  attr_accessor :password
+  before_save :encrypt_password
+  
+  validates_confirmation_of :password
+  validates_length_of :password, minimum: 8
+  validates_presence_of :password, :on => :create
+  validates_presence_of :email
+  validates_uniqueness_of :email
 end
+
