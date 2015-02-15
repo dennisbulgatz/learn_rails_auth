bd763ce12ce299caa4285a8c5b51d4e8a9748db8
Adam Bulgatz <adam@bulgatz.com>
2015-02-15 03:33:17 +0000

    Removed deprecated 'attr_accessible ' from user model, whitelisted params in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ def new
 end
 
 def create
-  @user = User.new(params[:user])
+  @user = User.new(user_params)
   if @user.save
     redirect_to root_url, :notice => "Signed up!"
   else
@@ -12,5 +12,9 @@ def create
   end
 end
 
+private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 
 end
