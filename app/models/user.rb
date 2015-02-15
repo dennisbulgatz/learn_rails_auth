852547000996da97e23bef54f079d8ea245bde91
Adam Bulgatz <adam@bulgatz.com>
2015-02-15 03:50:20 +0000

    added encrypt method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ActiveRecord::Base
-  # attr_accessible :email, :password, :password_confirmation
-  
   attr_accessor :password
   before_save :encrypt_password
   
@@ -9,5 +7,12 @@ class User < ActiveRecord::Base
   validates_presence_of :password, :on => :create
   validates_presence_of :email
   validates_uniqueness_of :email
+
+  def encrypt_password
+  	if password.present?
+  		self.password_salt = BCrypt::Engine.generate_salt
+  		self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+  	end
+  end
 end
 
