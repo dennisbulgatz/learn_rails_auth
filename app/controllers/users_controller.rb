6c3336d837b3cd1d93ae621da14e5f73d2c505c7
vagrant <vagrant@vagrant.vm>
2015-02-19 05:44:30 +0000

    allow signup of new users, which was blocked by auth check

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,25 @@
 class UsersController < ApplicationController
-def new
-  @user = User.new
-end
+  #users should not be able to create their own accounts, but leave this for early development
+  skip_before_action :require_login, only: [:new, :create]
 
-def create
-  @user = User.new(user_params)
-  if @user.save
-    redirect_to root_url, :notice => "Signed up!"
-  else
-    render "new"
+  def new
+    @user = User.new
   end
-end
 
-def index
-  @users = User.all
-end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_url, :notice => "Signed up!"
+    else
+      render "new"
+    end
+  end
 
-private
+  def index
+    @users = User.all
+  end
+
+  private
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation)
   end
